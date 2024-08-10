c98bb608f4d9756080ef0db2fef87d9c98f81c8a
Ryan H <ryanzhutch@gmail.com>
2024-08-09 18:36:20 -0600

    chore: Add favicon to the app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '../css/main.css';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/images/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
